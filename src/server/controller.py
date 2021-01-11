b6b1b34d60ad547383f6ea8500e03e652fe89239
Benjamin Bulis <beni1999@gmx.at>
2021-01-11 22:53:30 +0100

    Added Comments to Server side

--- src/server/controller.py
+++ src/server/controller.py
@@ -2,14 +2,30 @@ from flask import Flask, request, jsonify
 from model.language_service import LanguageDetect
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False    # stop sorting keys for returning
 language = LanguageDetect()
 
+"""
+Server for detecting languages
 
-@app.route('/lg')
+:author: Benjamin Bulis
+:version: V1.0
+"""
+
+
+@app.route('/lg')   # defining route for server app
 def hello():
-    id = request.args.get("id")
+    """
+    Detecting the given languages and returning an JSON object
+    returning error message if no input is given
+
+    :return: return json object with requestet data
+    """
+    id = request.args.get("id")  # getting argument from url
+    if id is None:
+        return jsonify({"success": False, "error": "no parameter id given"})
     if id == "":
-        return jsonify({"success": "false", "error": "no text given"})
+        return jsonify({"success": False, "error": "no text given"})
     result = language.detect_language(id)
     return jsonify({"success": True, "result": result})
 
--- src/server/model/language_service.py
+++ src/server/model/language_service.py
@@ -3,16 +3,28 @@ from iso639 import languages
 
 
 class LanguageDetect:
+    """
+    service for detecting languages and returning object
+
+    :autor: Benjamin Bulis
+    :version: V1.0
+    """
 
     def detect_language(self, input):
+        """
+        detecting langauge and manage other return parameters
+
+        :param input: input text from controller
+        :return: object - reliability, language, short name. probability
+        """
         detect_result = detect_langs(input)
-        split_result = str(detect_result[0]).split(":")
+        split_result = str(detect_result[0]).split(":")     # spliting string in to array entries
 
 
-        long_name = languages.get(alpha2=split_result[0]).name
-        reliablie = False if float(split_result[1]) <= 0.8 else True
+        long_name = languages.get(alpha2=split_result[0]).name      # getting long name of language
+        reliable = False if float(split_result[1]) <= 0.8 else True        # setting reliability
         short_name = split_result[0]
         prob = split_result[1]
 
-        return {'language': long_name, 'short': short_name, "prob": prob, "reliable": reliablie}
+        return {"reliable": reliable, 'language': long_name, 'short': short_name, "prob": prob}
 
