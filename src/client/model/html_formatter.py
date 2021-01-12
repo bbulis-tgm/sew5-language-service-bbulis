5c75be710227a8707df6f88b2d8d3ac1d5891a13
Benjamin Bulis <beni1999@gmx.at>
2021-01-12 12:24:46 +0100

    Fixed Issue with getting parameters Getting Key:Value pairs and not array points Error when converting string to float and backwards

--- src/client/model/html_formatter.py
+++ src/client/model/html_formatter.py
@@ -7,13 +7,15 @@ class HtmlFormatter:
         :param json: json object which is used for formating
         :return ret: html string for printing to output field
         """
-        reliable = json['result'][0]
-        language = json['result'][1]
-        short = json['result'][2]
-        prob = json['result'][3]
+        print(json)
+        reliable = json['result']['reliable']
+        language = json['result']['language']
+        short = json['result']['short']
+        prob = json['result']['prob']
+        prob_out = round(float(prob)*100,2)
 
         ret = "reliable: " + "<b>yes</b>" if reliable else "<b>no</b>"
         ret += "<br>language: <b>" + str(language) + "</b>"
-        ret += "<br>probability: <b>" + "{:.2f}".format(prob) + "</b>"
+        ret += "<br>probability: <b>" + str(prob_out) + "</b>"
 
         return ret
