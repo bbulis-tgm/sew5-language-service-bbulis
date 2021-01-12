0f6e3d435ac895e2bfb1246b53350c49b85d5dc9
Benjamin Bulis <beni1999@gmx.at>
2021-01-12 10:55:02 +0100

    working on displaying result to the outputfield and sending to server for detection

--- src/client/controller.py
+++ src/client/controller.py
@@ -6,11 +6,39 @@ from model.language_request import LanguageRequest
 
 class Controller(QtWidgets.QMainWindow, Ui_MainWindow):
     """
+    controller connects view gui and model logic
 
     :author: Benjamin Bulis
     :version: V1.0
     """
-    pass
+
+    def check_language(self):
+        """
+        checks language and connects to the language service
+
+        :return: no return value
+        """
+        pass
+
+    def display_output(self, output: str):
+        """
+        it displays output to the output field
+
+        :param output: output string
+        :return:
+        """
+        self.ui.outputField.clear()
+        self.ui.outputField.append(output)
+
+    def __init__(self):
+        """
+        init methode starts gui
+        """
+        super().__init__(None)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.checkButton.clicked.connect(self.check_language)
 
 
 if __name__ == "__main__":
