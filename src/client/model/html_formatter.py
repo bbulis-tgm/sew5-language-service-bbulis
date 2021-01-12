86a226ff051d4eedab7ae7d74e342110fe7b449d
Benjamin Bulis <beni1999@gmx.at>
2021-01-12 11:15:08 +0100

    Added Comments and renamed methode name Added Returning of html code

--- src/client/model/html_formatter.py
+++ src/client/model/html_formatter.py
@@ -1,12 +1,19 @@
 class HtmlFormatter:
 
-    def __init__(self, json):
+    def formatter(self, json):
         """
         gets json object and returns html string fot printing to output-field
 
-        :param json:
+        :param json: json object which is used for formating
+        :return ret: html string for printing to output field
         """
         reliable = json['result'][0]
         language = json['result'][1]
         short = json['result'][2]
         prob = json['result'][3]
+
+        ret = "reliable: " + "<b>yes</b>" if reliable else "<b>no</b>"
+        ret += "<br>language: <b>" + str(language) + "</b>"
+        ret += "<br>probability: <b>" + "{:.2f}".format(prob) + "</b>"
+
+        return ret
